a81be346a35c50b5486d942439d4d3c049cb675e
Jaryd Hunter <jaryd.hunter@sickkids.ca>
2019-07-04 15:10:46 -0400

    commented out some characters at the bottom of evaluateClustering.R, I can't tell what they are there for my best guess is it was a comment that lost it's # in front

--- Code/Tests/evaluateClustering.R
+++ Code/Tests/evaluateClustering.R
@@ -15,7 +15,7 @@ evaluateClustering <- function(cluster, data, completeLabels,
   acc <- calAcc(completeLabels, labels)
   nmi <- calNMI(completeLabels, labels)
   surv <- evaluateSurvival(clinicalData, labels)
-#     
+#
 #   # Evaluate the new labels on individuals in the intersection
 #   intLabels <- labels[intersectInd]
 #   intCompleteLabels <- completeLabels[intersectInd]
@@ -26,11 +26,14 @@ evaluateClustering <- function(cluster, data, completeLabels,
 #     intSurv <- c(NA, NA)
 #   } else {
 #     intSurv <- evaluateSurvival(intClinicalData, intLabels)
-#   }  
+#   }
   results <- c(acc, nmi, surv) #intAcc, intNmi, intSurv, runTime)
-  
+
   return(results)
 }
-pathway analysis
-real data set run clustering, survival analysis, 
-send link
+##### Comments below was just in the script, not sure why, commenting out
+##### because it causes errors, when sourcing this file, but it could be the
+##### code didn't upload properly and there is more missing so it is now incorrect.
+#pathway analysis
+#real data set run clustering, survival analysis,
+#send link
